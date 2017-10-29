074647e815ff5f1449872e0c1c3cf6c5e828013a
aegoose <aegoose@126.com>
2017-10-29 14:27:08 +0800

    update rails to 5.1.4

--- qiniu_js_rails.gemspec
+++ qiniu_js_rails.gemspec
@@ -22,7 +22,7 @@ Gem::Specification.new do |s|
   s.executables  = `git ls-files -- bin/*`.split("\n").map { |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_dependency "rails", "~> 5.0.2"
+  s.add_dependency "rails", '~> 5.1.4'
   s.add_dependency "plupload-rails"
   s.add_dependency "qiniu"
 
---
+++ test/qiniu_js_rails_styles1_test.rb
@@ -0,0 +1,17 @@
+
+require 'test_helper'
+
+class QiniuJsRailsStylesTest < ActiveSupport::TestCase
+
+  # test "product should not raise aa error when empty" do
+  #   p = Product.new
+  #   p.id = 1
+  #   p.images = "kkk"
+  #   # puts p.images_keys.map { |x| p.images_small_urls = x }
+  #   # puts p.methods.grep(/images_/)
+
+  #   puts p.images_small_url
+
+  # end
+
+end
